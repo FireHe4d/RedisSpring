8a90356e3363290cf997dfcda1ad3bd391ea0c54
Ayanbek <aianbek.01@mail.r>
2022-10-10 00:01:41 +0600

    first commit

---
+++ src/main/java/com/example/demo/repository/UserDao.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    boolean saveUser(User user);
+
+    List<User> fetchAllUser();
+
+    User fetchUserById(Long id);
+
+    boolean deleteUser(Long id);
+
+    boolean updateUser(Long id, User user);
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    boolean saveUser(User user);
+
+    List<User> fetchAllUser();
+
+    User fetchUserById(Long id);
+
+    boolean deleteUser(Long id);
+
+    boolean updateUser(Long id, User user);
+}
